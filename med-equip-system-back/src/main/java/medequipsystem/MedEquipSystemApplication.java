f572f737790d6b2e302c45ed69ad365dc15abe7d
MiljanaMa <miljana.marjanovic9@gmail.com>
2023-11-09 14:54:53 +0100

    change spring boot version

---
+++ med-equip-system-back/src/main/java/medequipsystem/MedEquipSystemApplication.java
@@ -0,0 +1,13 @@
+package medequipsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedEquipSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedEquipSystemApplication.class, args);
+	}
+
+}
---
+++ med-equip-system-back/src/test/java/medequipsystem/MedEquipSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package medequipsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedEquipSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
