38d8e8d72f472b68922b455610ed5d211709ca2b
milena <milenamarkovickg@gmail.com>
2023-12-18 22:54:50 +0100

    bugfix: fix auth guard

--- med-equip-system-front/src/app/auth/guard/auth.guard.ts
+++ med-equip-system-front/src/app/auth/guard/auth.guard.ts
@@ -2,21 +2,27 @@ import { Injectable } from "@angular/core";
 import { CanActivate, Router } from "@angular/router";
 import { AuthService } from "../auth.service";
 import { CurrentUser } from "../model/current-user.model";
-import { Observable } from "rxjs";
+import { Observable, map, take } from "rxjs";
+import { __await } from "tslib";
 
 @Injectable({
     providedIn: 'root',
   })
   export class AuthGuard implements CanActivate {
     constructor( private router: Router, private authService: AuthService) {}
-  
-    canActivate(): boolean{
-          
-      const user: Observable<CurrentUser> = this.authService.getCurrentUser();
-      if (user) {
-        this.router.navigate(['/login']);
-        return false;
+
+    canActivate(): Observable<boolean> {
+        return  this.authService.getCurrentUser().pipe(
+          take(1),
+          map((user: CurrentUser | null) => {
+            if (user && user.email !== '') {
+              return true; 
+            } else {
+              this.router.navigate(['/login']);
+              return false;
+            }
+          })
+        );
       }
-      return true;
-    }
-  }
+      
+}
